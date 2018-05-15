f9a37d813d9372ce11861ec18e0b99f774de3d91
Dmitri Larionov <larionovdmitri@gmail.com>
2018-05-15 16:05:04 -0500

    template content

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Componentgit } from 'react'
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Template Heading</h1>
+            </div>        
+        )
+    }
+}
+export default App
+
